442ab1560dc2693934883dbc464aa4da404ad49c
Kenyi De la cruz <kenyi_dlcp_93@outlook.com>
2022-07-03 20:27:42 -0500

    feat:BCPUDEMY add field port

--- src/main/java/com/formacionbdi/springboot/app/productos/springbootservicioproductos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/springbootservicioproductos/controllers/ProductoController.java
@@ -3,25 +3,40 @@ package com.formacionbdi.springboot.app.productos.springbootservicioproductos.co
 import com.formacionbdi.springboot.app.productos.springbootservicioproductos.models.entity.Producto;
 import com.formacionbdi.springboot.app.productos.springbootservicioproductos.models.service.IProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProductoController {
+
+  @Autowired
+  private Environment environment;
+
+  @Value("${server.port}")
+  private Integer port;
 
   @Autowired
   private IProductoService productoService;
 
   @GetMapping("/listar")
   public List<Producto> listar() {
-    return productoService.findAll();
+    return productoService.findAll().stream().map(p -> {
+      p.setPort(port);
+      return p;
+    }).collect(Collectors.toList());
   }
 
   @GetMapping("/ver/{id}")
   public Producto detalle(@PathVariable Long id) {
-    return productoService.findById(id);
+    Producto producto = productoService.findById(id);
+    //producto.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+    producto.setPort(port);
+    return producto;
   }
 }
